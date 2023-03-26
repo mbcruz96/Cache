4539c83d081af9049ea3ef1f83c1749b3dd46d93
Taylor Laird <taylorolaird@knights.ucf.edu>
2023-03-26 17:25:50 -0400

    add framework for reding in the file input

--- sim/cacheSim.c
+++ sim/cacheSim.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include "ourHeaders.h"
 
-int main(void)
+// grab arguments from command line
+int main(int argc, char *argv[])
 {
+    // check for correct number of arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./cacheSim <tracefile>\n");
+        return 1;
+    }
+    // open file
+    FILE *file = fopen(argv[1], "r");
+    // check if file opened
+    if (file == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+    // read file
+    char line[256];
+    while (fgets(line, sizeof(line), file))
+    {
+        printf("%s", line);
+    }
+    // close file
+    fclose(file);
+    
+    // call hello_world function from the header file
     hello_world();
 }
