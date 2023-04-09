5d42b820a828c7c14e1eb0e0592de30bcc5f3d9c
Taylor Laird <taylorolaird@knights.ucf.edu>
2023-04-08 21:02:27 -0400

    added some file io boiler plate, and to track branch

--- sim/cacheSim.c
+++ sim/cacheSim.c
@@ -18,12 +18,50 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
+    
+    int cacheSize = 0;
+    int assoc = 0;
+    int blockSize = 0;
+    int replacementPolicy = 0;
+
+    // TODO: add in safeguards for bad input, 
+    //  blocksizes that are not powers of 2,
+    //  setsizes that are not powers of 2
+
+    // Gets the input from the user
+    printf("CACHE SIZE?: \n");
+    scanf("%d", &cacheSize);
+
+    printf("ASSOC?: \n");
+    scanf("%d", &assoc);
+
+    printf("BLOCKSIZE?: \n");
+    scanf("%d", &blockSize);
+    
+    printf("Replacement policy?: \nLRU = 1\nFIFO = 2\nOptimal = 3\n");
+    scanf("%d", &replacementPolicy);
+
     // read file
     char line[256];
     while (fgets(line, sizeof(line), file))
     {
         printf("%s", line);
     }
+
+    // print out the input
+    printf("SIZE: %d\n", cacheSize);
+    printf("ASSOC: %d\n", assoc);
+    printf("BLOCKSIZE: %d\n", blockSize);
+    if (replacementPolicy == 1) {
+        printf("Replacement policy: LRU\n");
+    }
+    else if (replacementPolicy == 2) {
+        printf("Replacement policy: FIFO\n");
+    }
+    else if (replacementPolicy == 3) {
+        printf("Replacement policy: Optimal\n");
+    }
+
     // close file
     fclose(file);
     
