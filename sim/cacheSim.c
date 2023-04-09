772b96e0b00b1562d46e0887849274d339c1a1fe
Taylor Laird <taylorolaird@knights.ucf.edu>
2023-04-09 13:55:19 -0400

    added getting valid user input

--- sim/cacheSim.c
+++ sim/cacheSim.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "ourHeaders.h"
+
+// prototype for is power of 2 function
+bool isPowerOfTwo(int x);
+void getInputs();
+
+// global variables for cache size, associativity, block size, and replacement policy
+int CAHCE_SIZE = -1;
+int ASSOCIATIVITY = -1;
+int BLOCK_SIZE = -1;
+int REPLACEMENT_POLICY = -1;
 
 // grab arguments from command line
 int main(int argc, char *argv[])
 {
     // check for correct number of arguments
+    // this is left as a command line argument for 
+    // ease of use
     if (argc != 2)
     {
-        printf("Usage: ./cacheSim <tracefile>\n");
+        printf("Usage: ./cacheSim <tracefile path>\n");
         return 1;
     }
     // open file
@@ -18,47 +32,31 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
-    
-    int cacheSize = 0;
-    int assoc = 0;
-    int blockSize = 0;
-    int replacementPolicy = 0;
 
     // TODO: add in safeguards for bad input, 
-    //  blocksizes that are not powers of 2,
-    //  setsizes that are not powers of 2
-
-    // Gets the input from the user
-    printf("CACHE SIZE?: \n");
-    scanf("%d", &cacheSize);
-
-    printf("ASSOC?: \n");
-    scanf("%d", &assoc);
-
-    printf("BLOCKSIZE?: \n");
-    scanf("%d", &blockSize);
+    //  blocksizes that are not powers of 2
     
-    printf("Replacement policy?: \nLRU = 1\nFIFO = 2\nOptimal = 3\n");
-    scanf("%d", &replacementPolicy);
+    // get inputs from user
+    getInputs();
 
     // read file
-    char line[256];
-    while (fgets(line, sizeof(line), file))
-    {
-        printf("%s", line);
-    }
+    // char line[256];
+    // while (fgets(line, sizeof(line), file))
+    // {
+    //     printf("%s", line);
+    // }
 
     // print out the input
-    printf("SIZE: %d\n", cacheSize);
-    printf("ASSOC: %d\n", assoc);
-    printf("BLOCKSIZE: %d\n", blockSize);
-    if (replacementPolicy == 1) {
+    printf("SIZE: %d\n", CAHCE_SIZE);
+    printf("ASSOC: %d\n", ASSOCIATIVITY);
+    printf("BLOCKSIZE: %d\n", BLOCK_SIZE);
+    if (REPLACEMENT_POLICY == 1) {
         printf("Replacement policy: LRU\n");
     }
-    else if (replacementPolicy == 2) {
+    else if (REPLACEMENT_POLICY == 2) {
         printf("Replacement policy: FIFO\n");
     }
-    else if (replacementPolicy == 3) {
+    else if (REPLACEMENT_POLICY == 3) {
         printf("Replacement policy: Optimal\n");
     }
 
@@ -67,4 +65,101 @@ int main(int argc, char *argv[])
     
     // call hello_world function from the header file
     hello_world();
+}
+
+// function to check if a number is a power of 2
+bool isPowerOfTwo(int x)
+{
+    // 8 is 1000
+    // 7 is 0111, 
+    // so 8 & 7 = 0
+
+    // 7 is 0111
+    // 6 is 0110
+    // so 7 & 6 = 6 or essentiall not 0
+    
+    return !(x & (x - 1));
+}
+
+// function to get the inputs from the user
+void getInputs() {
+    int inputSize = 256;
+    char input [inputSize];
+
+    // CACHE SIZE
+    while (true)
+    {
+        printf("Cache Size?: \n");
+        if (fgets (input, inputSize, stdin) == NULL) {
+            printf(">>> Failed to read Cache Size\n");
+        }
+        // convert input string to int
+        CAHCE_SIZE = atoi (input);
+        // safeguard for bad input
+        if (CAHCE_SIZE <= 0) {
+            printf(">>> Must be a positive integer greater than 1\n");
+        }
+        else if (!isPowerOfTwo(CAHCE_SIZE)) {
+            printf(">>> Must be a power of 2\n");
+        }
+        else {
+            break;
+        }
+    }
+    // Associativity
+    while (true)
+    {
+        printf("Associativity: \n");
+        if (fgets (input, inputSize, stdin) == NULL) {
+            printf(">>> Failed to read Associativity\n");
+        }
+        // convert input string to int
+        ASSOCIATIVITY = atoi (input);
+        // safeguard for bad input
+        if (ASSOCIATIVITY <= 0) {
+            printf(">>> Must be a positive integer greater than 1\n");
+        }
+        else if (!isPowerOfTwo(ASSOCIATIVITY)) {
+            printf(">>> Must be a power of 2\n");
+        }
+        else {
+            break;
+        }
+    }
+    // Block Size
+    while (true)
+    {
+        printf("Block Size: \n");
+        if (fgets (input, inputSize, stdin) == NULL) {
+            printf(">>> Failed to read Block Size\n");
+        }
+        // convert input string to int
+        BLOCK_SIZE = atoi (input);
+        // safeguard for bad input
+        if (BLOCK_SIZE <= 0) {
+            printf(">>> Must be a positive integer greater than 1\n");
+        }
+        else if (!isPowerOfTwo(BLOCK_SIZE)) {
+            printf(">>> Must be a power of 2\n");
+        }
+        else {
+            break;
+        }
+    }
+    while (true)
+    {
+        printf("Replacement policy?: \nLRU = 1\nFIFO = 2\nOptimal = 3\n");
+        if (fgets (input, inputSize, stdin) == NULL) {
+            printf(">>> Failed to read Replacement Policy\n");
+        }
+        // convert input string to int
+        REPLACEMENT_POLICY = atoi (input);
+        // safeguard for bad input
+        if (REPLACEMENT_POLICY == 1 || REPLACEMENT_POLICY == 2 || REPLACEMENT_POLICY == 3) {
+            break;
+        }
+        else {
+            printf(">>> Invalid selection, must be 1, 2, or 3\n");
+        }
+    }
 }
